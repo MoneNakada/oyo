5f08cac81d45b5205eb55fa4738974c74ccb67f8
EC2 Default User <ec2-user@ip-172-31-10-198.ec2.internal>
2022-07-16 15:07:01 +0000

    8d

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,13 @@ class BooksController < ApplicationController
   end
 
   def index
-    @books = Book.all.sort {|a,b| b.favorites.count <=> a.favorites.count}
+    if params[:latest]
+      @books = Book.latest
+    elsif params[:star_count]
+      @books = Book.star_count
+    else
+      @books = Book.all
+    end
     @book = Book.new
   end
 
